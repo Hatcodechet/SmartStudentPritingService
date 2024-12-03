5d46b8a50c99cb968222f65cc807f447c2e938f4
Hưng Phan <phanphuochung.07022004@gmail.com>
2024-12-03 18:53:02 +0700

    update sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,67 +1,41 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import { IoDiamondOutline, IoAdd } from "react-icons/io5";
-import PrintPopup from "../PopUpManage/PrintPopup";
-import CreatePrintJob from "../PopUpManage/CreatePrintJob";  
+// import PrintPopup from "../PopUpManage/PrintPopup";
+// import CreatePrintJob from "../PopUpManage/CreatePrintJob";
 import { useNavigate } from "react-router-dom";
 const Header: React.FC = () => {
-    const navigate = useNavigate();
-    // const [isModalOpen, setIsModalOpen] = useState(false);
-    // const [showPrinterSelection, setShowPrinterSelection] = useState(false);  
+  const navigate = useNavigate();
+  const handlePrinting = () => {
+    navigate("/services/printing");
+  };
 
-    // const openModal = () => setIsModalOpen(true);
-    // const closeModal = () => setIsModalOpen(false);
-
-    // const handleContinue = () => {
-    //     setIsModalOpen(false); 
-    //     setShowPrinterSelection(true); 
-    // };
-
-    // const handleBackToUpload = () => {
-    //     setShowPrinterSelection(false);  
-    //     setIsModalOpen(true);  
-    // };
-
-    const handlePrinting = () => {
-        navigate('/services/printing');
-    }
-
-    return (
-        <>
-            <header className="flex items-center justify-between px-5 py-4 w-full border-b border-slate-300">
-                <div className="flex items-center w-2/3 space-x-10">
-                    <span className="text-black">Bảng điều khiển</span>
-                    <input
-                        type="text"
-                        placeholder="Tìm kiếm"
-                        className="border border-gray-300 rounded-md p-1 w-1/2 text-sm bg-white"
-                    />
-                </div>
-                <div className="flex space-x-4">
-                    <button className="font-medium text-blue-500 bg-blue-100 hover:bg-blue-300 py-1 px-4 rounded-md flex flex-row items-center gap-x-2">
-                        <IoDiamondOutline />
-                        Thêm số dư
-                    </button>
-                    <button
-                        onClick={handlePrinting}
-                        className="font-medium bg-blue-500 hover:bg-blue-300 text-white py-1 px-4 rounded-md flex flex-row items-center gap-x-2"
-                    >
-                        <IoAdd />
-                        Tạo lệnh in
-                    </button>
-                </div>
-            </header>
-
-            {/* {isModalOpen && (
-                <PrintPopup
-                    isOpen={isModalOpen}
-                    onClose={closeModal}
-                    onContinue={handleContinue}
-                />
-            )}
-
-            {showPrinterSelection && <CreatePrintJob onBack={handleBackToUpload} />} */}
-        </>
-    );
+  return (
+    <>
+      <header className="flex items-center justify-between px-5 py-4 w-full border-b border-slate-300">
+        <div className="flex items-center w-2/3 space-x-10">
+          <span className="text-black">Bảng điều khiển</span>
+          <input
+            type="text"
+            placeholder="Tìm kiếm"
+            className="border border-gray-300 rounded-md p-1 w-1/2 text-sm bg-white"
+          />
+        </div>
+        <div className="flex space-x-4">
+          <button className="font-medium text-blue-500 bg-blue-100 hover:bg-blue-300 py-1 px-4 rounded-md flex flex-row items-center gap-x-2">
+            <IoDiamondOutline />
+            Thêm số dư
+          </button>
+          <button
+            onClick={handlePrinting}
+            className="font-medium bg-blue-500 hover:bg-blue-300 text-white py-1 px-4 rounded-md flex flex-row items-center gap-x-2"
+          >
+            <IoAdd />
+            Tạo lệnh in
+          </button>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,45 +1,138 @@
-import React from 'react';
-import Logo from '../../public/favicon-32x32.png';
-import { CiHome } from "react-icons/ci";
-import { useAuth } from '../context/AuthContext';
-
-const SideBar: React.FC = () => {
-    const {logout} = useAuth();
-    return (
-        <div className="w-1/4 flex flex-col min-h-0">
-
-            <div className="max-h-[30%] w-full flex flex-row justify-between border border-r-slate-300 p-3">
-                <img src={Logo} alt="Bach Khoa Logo" />
-                <div className="flex flex-col items-center justify-center">
-                    <p className="text-[0.7rem]">Ho Chi Minh University of Technology</p>
-                    <p className="text-[0.9rem] font-bold text-blue-400">Student Smart Printing Service</p>
-                </div>
-            </div>
-
-
-            <nav className="flex-grow w-full bg-blue-500 text-white flex flex-col p-5 space-y-6">
-                <a href="#" className="flex items-center space-x-3"> 
-                    <CiHome />
-                    <span>Bảng điều khiển</span>
-                </a>
-                <a href="#" className="flex items-center space-x-3">
-                    <CiHome />
-                    <span>Lịch sử lệnh in</span>
-                </a>
-                <a href="#" className="flex items-center space-x-3">
-                    <CiHome />
-                    <span>Danh sách máy in</span>
-                </a>
-                <a 
-                href="/" 
-                onClick={logout}
-                className="flex items-center space-x-3">
-                    <CiHome />
-                    <span>Đăng xuất</span>
-                </a>
-            </nav>
+import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+const Sidebar: React.FC = () => {
+  const { user } = useAuth();
+  return (
+    <div
+      className="d-flex flex-column align-seft-start h-full align-items-start"
+      // style={{ alignSelf: "start", height: "100%" }}
+    >
+      <div
+        className="d-flex flex-column flex-shrink-0 h-full w-[8rem]  md:w-48 "
+        style={{ backgroundColor: "#1E90FF" }}
+      >
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-7 w-7"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content lg:text-white text-black mt-3 z-[1] p-2 shadow bg-[#F2F0F0] rounded-box w-52"
+            >
+              <li className="m-3">
+                <Link to="/">Trang chủ</Link>
+              </li>
+              <li className="m-3">
+                <summary>Dịch vụ</summary>
+                <ul className="p-2 z-50 text-black">
+                  <li>
+                    <Link to="/services/printing">In tài liêụ</Link>
+                  </li>
+                  {(user?.role === "admin" || !user) && (
+                    <li>
+                      <Link to="/services/management">Quản lý máy in</Link>
+                    </li>
+                  )}
+                </ul>
+              </li>
+              <li className="m-3">
+                <Link to="/about">Giới thiệu</Link>
+              </li>
+            </ul>
+          </div>
+          {/* <Link
+            to="/"
+            className="btn btn-ghost text-xl"
+            style={{ paddingLeft: "0" }}
+          >
+            <img
+              src="https://quanlycongnghiep.com/wp-content/uploads/2019/06/logo-hcmut.png"
+              alt="logo"
+              className="h-10 w-10"
+            ></img>
+          </Link> */}
         </div>
-    );
+
+        <div className="navbar-center hidden lg:flex text-white text-lg">
+          <ul className=" px-1 menu menu-vertical">
+            <li className="m-3">
+              <Link to="/">Trang chủ</Link>
+            </li>
+            <li tabIndex={0} className="m-3">
+              <details>
+                <summary>Dịch vụ</summary>
+                <ul className="p-1 bg-[#154569] z-50 w-36">
+                  {user ? (
+                    <li>
+                      <Link to="/services/printing">In tài liệu</Link>
+                    </li>
+                  ) : (
+                    <li>
+                      <Link to="/login">In tài liệu</Link>
+                    </li>
+                  )}
+                  {user?.role === "admin" || !user ? (
+                    <li>
+                      <Link to="/services/management">Quản lý máy in</Link>
+                    </li>
+                  ) : !user ? (
+                    <li>
+                      <Link to="/login">Quản lý máy in</Link>
+                    </li>
+                  ) : null}
+                </ul>
+              </details>
+            </li>
+            <li className="m-3">
+              <Link to="/about">Giới thiệu</Link>
+            </li>
+          </ul>
+        </div>
+        {/* <div className="navbar-end">
+          {user ? (
+            <div className="dropdown dropdown-end">
+              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Avatar"
+                    src="https://i1.sndcdn.com/artworks-G0XZmnzGNxLfoZx2-s2zyrw-t500x500.jpg"
+                  />
+                </div>
+              </label>
+              <ul
+                tabIndex={0}
+                className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-white rounded-box w-52"
+              >
+                <li>
+                  <a href="/" onClick={logout}>
+                    Logout
+                  </a>
+                </li>
+              </ul>
+            </div>
+          ) : (
+            <Link to="/login" className="btn bg-[#1E90FF] text-white">
+              Đăng nhập
+            </Link>
+          )}
+        </div> */}
+      </div>
+    </div>
+  );
 };
 
-export default SideBar;
+export default Sidebar;
